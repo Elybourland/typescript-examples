20f68f5d2a89b5b418b74911f3b523e88a5c9722
Elybourland <JEBourland@yahoo.com>
2022-06-28 14:29:10 -0700

    delayed initialization

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -44,3 +44,16 @@ const logNumber: (i: number) => void = (i: number) => {
 const json = '{"x": 10, "y": 20}';
 const coordinates: { x: number; y: number } = JSON.parse(json); // without annotation of number, you would get 'any' type
 console.log(coordinates);
+
+// (2) When we declare a variable on one line
+// and initialize it later
+// delayed initialization
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean; // needed to add boolean or initialize with false
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}

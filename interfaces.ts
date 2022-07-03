b9d3617157219c0dc2ce267ab496bec3a6b491b6
Elybourland <JEBourland@yahoo.com>
2022-07-03 14:44:51 -0700

    code reused with interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -14,6 +14,15 @@ const oldCivic = {
   }
 };
 
+const beverage = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
+}
+
 const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
@@ -21,3 +30,4 @@ const printSummary = (item: Reportable): void => {
 // oldCivic must have a summary function
 
 printSummary(oldCivic);
+printSummary(beverage);
